bdce03c3834234d7dd4c6ddab6c7572353dd46c4
vivsvaan <sharmavivsvaan@gmail.com>
2021-04-18 18:11:28 +0530

    updated client

--- client/client.py
+++ client/client.py
@@ -1,1 +1,19 @@
 # client.py
+import logging
+
+logging.getLogger()
+logging.basicConfig(filename='client_logs.txt', level=logging.DEBUG)
+
+logging.info('---------- STARTING CLIENT ----------')
+print('---------- STARTING CLIENT ----------')
+
+config = read_conf('client.conf')
+
+server_service = ServerService(config.server_host, config.server_port)
+
+
+
+
+
+
+
