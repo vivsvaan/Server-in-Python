768a93259cf0227a703e673e9758d72f718ed297
vivsvaan <sharmavivsvaan@gmail.com>
2021-05-10 10:52:51 +0530

    added message processing service

--- server/helper/services.py
+++ server/helper/services.py
@@ -51,3 +51,45 @@ class TCPService:
         except Exception as e:
             logging.error(e)
             raise e
+
+
+class MessageProcessingService:
+    def __init__(self, client_interface):
+        logging.info('Creating threads and queue for message processing...')
+        self.client_interface = client_interface
+        self.read_thread = multiprocessing.Process(target=self.read_message)
+        self.process_thread = multiprocessing.Process(target=self.process_message)
+        self.write_thread = multiprocessing.Process(target=self.send_message)
+        self.read_queue = multiprocessing.Queue(maxsize=READ_QUEUE_SIZE)
+        self.write_queue = multiprocessing.Queue(maxsize=WRITE_QUEUE_SIZE)
+        self._terminate = False
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self):
+        self.terminate()
+        self.join()
+
+    def start(self):
+        logging.info('Starting Threads...')
+        self.read_thread.start()
+        self.process_thread.start()
+        self.write_thread.start()
+
+    def terminate(self):
+        logging.info('Terminating Threads...')
+        self._terminate = True
+        if self.read_thread:
+            self.read_thread.terminate()
+        if self.process_thread:
+            self.process_thread.terminate()
+        if self.write_thread:
+            self.write_thread.terminate()
+
+    def join(self):
+        logging.info('Joining Threads')
+        self.read_thread.join()
+        self.process_thread.join()
+        self.write_thread.join()
